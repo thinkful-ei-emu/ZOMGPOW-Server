3e49d9ee7cad9f2da0a7eb99fcfd0425ed8d05a1
Nicholas Gunter <ygnick@live.com>
2019-09-19 12:31:33 -0400

    Added /register/student endpoint, updated readme

--- src/registration/registration-router.js
+++ src/registration/registration-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const RegistrationService = require('./registration-service');
+const { requireAuth } = require('../middleware/jwt-auth');
 
 const registrationRouter = express.Router();
 const jsonBodyParser = express.json();
@@ -29,7 +30,7 @@ registrationRouter
       if (hasTeacherWithEmail)
         return res.status(400).json({ error: 'Name already taken' });
 
-      const hashedPassword = await  RegistrationService.hashPassword(password);
+      const hashedPassword = await RegistrationService.hashPassword(password);
 
       const newUser = {
         full_name,
@@ -46,7 +47,51 @@ registrationRouter
         .status(201)
         .location(path.posix.join(req.originalUrl, `/${user.id}`))
         .json(RegistrationService.serializeTeacherUser(user));
-    } catch(error) {
+    } catch (error) {
+      next(error);
+    }
+  });
+
+registrationRouter
+  .post('/student', requireAuth, jsonBodyParser, async (req, res, next) => {
+
+    const { class_id, full_name } = req.body;
+
+    if (!class_id) {
+      return res.status(400).json({ error: 'Missing class id' })
+    }
+    if (!full_name) {
+      return res.status(400).json({ error: 'Missing full name' })
+    }
+
+    try {
+      let studentUserName = RegistrationService.createStudentUserName(full_name);
+
+      let nameTaken = await RegistrationService.verifyStudentUserName(
+        req.app.get('db'),
+        studentUserName
+      );
+
+      if (nameTaken) {
+        studentUserName = RegistrationService.generateNumForStudent(studentUserName);
+      }
+
+      const newUser = {
+        user_name: studentUserName,
+        full_name,
+        class_id,
+      }
+
+      const user = await RegistrationService.insertStudentUser(
+        req.app.get('db'),
+        newUser
+      );
+
+      res
+        .status(201)
+        .json(RegistrationService.serializeStudentUser(user))
+    }
+    catch (error) {
       next(error);
     }
   });
