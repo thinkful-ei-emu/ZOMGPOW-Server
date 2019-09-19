f82058f45209d23ca1f4b7ae35635e08727cde3d
Nicholas Gunter <ygnick@live.com>
2019-09-19 14:25:01 -0400

    Subgoal endpoint complete

--- src/subGoals/subGoal-service.js
+++ src/subGoals/subGoal-service.js
@@ -11,6 +11,11 @@ const subGoalService ={
       .returning('*')
       .then(([subGoal])=> subGoal);
   },
+  verifyStudentGoal(db, id){
+    return db('student_goals')
+      .where('student_goal_id', id)
+      .first();
+  }
 };
 
 module.exports = subGoalService;
