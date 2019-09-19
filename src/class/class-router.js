b7967fd271f793f31d65137365c391a5c131b309
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-09-19 16:15:58 -0400

    implemented class service route to get all students by classid

--- src/class/class-router.js
+++ src/class/class-router.js
@@ -8,7 +8,7 @@ const { requireAuth } = require('../middleware/jwt-auth');
 
 classRouter
   .route('/')
-  .all(requireAuth)   
+  .all(requireAuth)
   .get((req, res, next) => {
     console.log(req.user)
     ClassService.getClasses(req.app.get('db'), req.user.id)
@@ -16,7 +16,32 @@ classRouter
         console.log(classes);
         return res.json(classes);
       })
-      .catch(next); 
+      .catch(next);
+  });
+
+classRouter
+  .route('/:class_id/students')
+  .all(requireAuth)
+  .all((req, res, next) => {
+    const { class_id } = req.params;
+    console.log('class_id in params', class_id)
+
+    ClassService.getByClassId(req.app.get('db'), class_id)
+      .then(singleClass => {
+        if (!singleClass) {
+          return res.status(404).json({
+            error: { message: `Class not found` }
+          });
+        }
+        console.log(res.singleClass)
+        res.singleClass = singleClass;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    console.log('single class', res.singleClass);
+    res.json(res.singleClass);
   });
 
 module.exports = classRouter;
