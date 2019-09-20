dc9615c9358c47add8ef88e53223bf281587827a
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-09-20 13:29:35 -0400

    Changing getStudentsByClassId to include goals again

--- src/class/class-service.js
+++ src/class/class-service.js
@@ -18,9 +18,18 @@ const ClassService = {
   },
   getStudentsByClassId(knex, id) {
     return knex
-      .from('students')    
-      .select('*')
-      .where('students.class_id', id);      
+      .from('students')
+      .leftJoin('student_goals', 'student_goals.student_id', 'students.id')
+      .leftJoin('goals', 'goals.id', 'student_goals.goal_id')
+      .leftJoin('subgoals', 'subgoals.student_goal_id', 'student_goals.id')
+      .select(
+        'students.full_name',
+        'students.user_name',
+        'goals.goal_title AS goal',
+        'student_goals.iscomplete',
+        'subgoals.goal_title As subgoal'
+      )
+      .where('students.class_id', id);
   },
   insertClass(knex, newClass) {
     return knex
