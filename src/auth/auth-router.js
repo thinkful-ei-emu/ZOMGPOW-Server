01fcc043dc8e1e04cf3a31e55240e9f1930e6ded
Nicholas Gunter <ygnick@live.com>
2019-09-20 17:00:37 -0400

    to merge

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const authRouter = express.Router();
 const AuthService = require('./auth-service');
 const jsonBodyParser = express.json();
-const { requireAuth }= require('../middleware/jwt-auth');
+const { requireAuth } = require('../middleware/jwt-auth');
 
 authRouter
   .post('/teacher/login', jsonBodyParser, (req, res, next) => {
@@ -50,40 +50,39 @@ authRouter
           });
       })
       .catch(next);
-      
+
   });
 
 authRouter
   .post('/student/login', jsonBodyParser, async (req, res, next) => {
     const { user_name } = req.body;
-    const loginStudent = { user_name };
-
-    for (const [key, value] of Object.entries(loginStudent))
-      if (value == null) {
-        return res.status(400).json({ error: `Missing '${key}' in request body` });
-      }
-    try{
-      const student = await AuthService.getStudentWithUsername(req.app.get('db'), loginStudent.user_name)
+    console.log(user_name)
+    const loginStudent = user_name;
+   
+    if (!loginStudent) {
+      return res.status(400).json({ error: `Missing user_name in request body` });
+    }
+    try {
+      const student = await AuthService.getStudentWithUsername(req.app.get('db'), loginStudent)
         .then(user => {
           if (!user) {
             return res.status(400).json({ error: 'Incorrect username' });
           }
-        console.log(user)      
-        return user;
-        
+          return user;
+
         });
-      const sub = loginStudent.user_name;
+      const sub = loginStudent;
       const payload = {
-        id:student.id,
+        id: student.id,
         user_name: student.user_name,
       };
       res.send({
-        authToken: AuthService.createJWT(sub,payload),
+        authToken: AuthService.createJWT(sub, payload),
       })
     } catch (error) {
       next(error)
-    } 
-  })    
+    }
+  })
   .put(requireAuth, (req, res) => {
     const sub = req.user.user_name;
     const payload = {
