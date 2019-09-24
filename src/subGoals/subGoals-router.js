83b6d741e1b4a92b927a552097517e1808dc18d1
Martha Plantz <maplantz89@gmail.com>
2019-09-24 10:52:12 -0700

    pulling dev

--- src/subGoals/subGoals-router.js
+++ src/subGoals/subGoals-router.js
@@ -9,7 +9,6 @@ const jsonBodyParser = express.json();
 subGoalRouter
   .post('/:student_goal_id', requireAuth, jsonBodyParser, async(req, res, next) => {
     const { goal_title, goal_description } = req.body
-
     const { student_goal_id } = req.params
 
     if(!goal_title){
@@ -20,34 +19,28 @@ subGoalRouter
     if(!goal_description){
       goal_description = null;
     }
-
     try {
       const isValidStudentGoal = await subGoalService.verifyStudentGoal(
         req.app.get('db'),
         student_goal_id
       );
-  
       if(!isValidStudentGoal){
         return res.status(404).json({error: 'Student goal not found'})
       }
-
       const newSubGoal = {
         student_goal_id,
         goal_title,
         goal_description
       };
-  
       const subGoal = await subGoalService.insertSubGoal(
         req.app.get('db'),
         newSubGoal
       );
-  
       res
         .status(201)
         .location(path.posix.join(req.originalUrl, `/${subGoal.id}`))
         .json({subGoal});
     }
-
     catch(error){
       next(error)
     }
