c2ab5dd5311a1ae9c54f72e1ef9f158d68d6b7bc
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-09-25 15:13:38 -0400

    added count for students in class

--- src/data/data-router.js
+++ src/data/data-router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const dataService = require('./data-service');
-const {requireAuth} = require('../middleware/jwt-auth');
+const { requireAuth } = require('../middleware/jwt-auth');
 
 const dataRouter = express.Router();
 const jsonBodyParser = express.json();
@@ -10,56 +10,48 @@ const jsonBodyParser = express.json();
 dataRouter
   .get('/:classId', requireAuth, async (req, res, next) => {
 
-      const {classId} = req.params
+    const { classId } = req.params
 
-      if(!classId){
-        return res.status(400).json({error: 'Need class id'})
-      }
+    if (!classId) {
+      return res.status(400).json({ error: 'Need class id' })
+    }
 
     try {
-    
-      const dates = await dataService.getTimeForGoal(req.app.get('db'), classId)
-      let time;
-      let created=''
-      let completed=''
-      let dateArr=[]
-     
-    
-      for(let i=0; i < dates.length; i++){
-     
 
-        // created = dates[i]["date_created"].toTimeString().slice(0,5)       
-        // completed = dates[i]["date_completed"].toTimeString().slice(0,5) 
-        let createdHours = Number(dates[i]["date_created"].getHours()) 
+      const dates = await dataService.getTimeForGoal(req.app.get('db'), classId)
+      const count = await dataService.getStudents(req.app.get('db'), classId)
+      console.log(count)
+      let time;     
+      let dateArr = []
+
+
+      for (let i = 0; i < dates.length; i++) {
+        let createdHours = Number(dates[i]["date_created"].getHours())
         let completedHours = Number(dates[i]["date_completed"].getHours())
         let createdMins = Number(dates[i]["date_created"].getMinutes())
-        let completedMins= Number(dates[i]["date_completed"].getMinutes())
-        
-        // time = Number(completed) - Number(created)
-        
-
-       let totalCreatedMins = (createdHours * 60) + createdMins
-       let totalCompletedMins = (completedHours * 60) + completedMins
-       let totalMins = totalCompletedMins - totalCreatedMins
-       let hours = Math.floor(totalMins/60).toString();
-       let mins = (totalMins % 60).toString()
-       if(hours < 10){
-        hours = `0${hours}`
-       }
-       if(mins < 10){
-        mins = `0${mins}`
-       }
-       time = `${hours}h ${mins}m`
-       dateArr.push({id: dates[i]["id"], goal_title: dates[i]["goal_title"], time: time, })
+        let completedMins = Number(dates[i]["date_completed"].getMinutes())
+        let totalCreatedMins = (createdHours * 60) + createdMins
+        let totalCompletedMins = (completedHours * 60) + completedMins
+        let totalMins = totalCompletedMins - totalCreatedMins
+        let hours = Math.floor(totalMins / 60).toString();
+        let mins = (totalMins % 60).toString()
+        if (hours < 10) {
+          hours = `0${hours}`
+        }
+        if (mins < 10) {
+          mins = `0${mins}`
+        }
+        time = `${hours}h ${mins}m`
+        dateArr.push({ id: dates[i]["id"], goal_title: dates[i]["goal_title"], time: time, })
       }
       console.log(time)
       console.log(dateArr);
 
-      res.status(200).json({dateArr})
+      res.status(200).json({ dateArr })
     }
-    catch(e){
+    catch (e) {
       next(e)
     }
   })
 
-  module.exports = dataRouter
+module.exports = dataRouter
--- src/data/data-service.js
+++ src/data/data-service.js
@@ -3,14 +3,20 @@ const config = require('../config');
 
 const dataService = {
   //how long it took a class to get a session goal
-  getTimeForGoal(db, class_id){
+  getTimeForGoal(db, class_id) {
     return db('goals')
       .select('id', 'goal_title', 'date_created', 'date_completed')
       .whereNotNull('date_completed')
-      .andWhere({class_id})
+      .andWhere({ class_id })
     // .where('class_id', class_id);
-     
+
   },
+
+  getStudents(db, class_id) {
+    return db('students')
+      .count('*')
+      .where({class_id});
+  }
 
 
 };
